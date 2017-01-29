70fed0121273b25ebc628079a67f1e80b74869a1
Ruhul Amin <r_amin105@yahoo.com>
2017-01-30 00:00:31 +0100

    New Media type movie added

--- app.js
+++ app.js
@@ -3,9 +3,12 @@ var playlist = new Playlist();
 var song = new Song('Coffee houser sei adda','Manna Dey','6:02');
 var song1 = new Song('Amar Hiar majhe lukiye ase','Madhurima Sen','5:04');
 
+var movie = new Movie('Moner Manus', 2010, '2:50:00');
+
 playlist.add(song);
 playlist.add(song1);
 
+playlist.add(movie);
 var playlistElement = document.getElementById("playlist");
 
 playlist.renderInElement(playlistElement);
